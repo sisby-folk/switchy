f107cbee8c0b142875aec052a4610c2e9f3c07c1
sisby-folk <nora.herrah.heartstrings@gmail.com>
2022-08-17 12:59:01 +1000

    Reorganize Commands. `set` on `new`.

--- src/main/java/folk/sisby/switchy/Switchy.java
+++ src/main/java/folk/sisby/switchy/Switchy.java
@@ -1,126 +1,17 @@
 package folk.sisby.switchy;
 
-import com.mojang.brigadier.arguments.StringArgumentType;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.nbt.NbtList;
-import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.Style;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
-import org.quiltmc.qsl.command.api.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Objects;
-
-import static net.minecraft.server.command.CommandManager.argument;
-import static net.minecraft.server.command.CommandManager.literal;
 
 public class Switchy implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger("Switchy");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-
-		CommandRegistrationCallback.EVENT.register((dispatcher, buildContext, environment) -> dispatcher.register(
-				literal("switchy")
-						.then(literal("help")
-								.executes((context) -> {
-									ServerPlayerEntity player = context.getSource().getPlayer();
-
-									inform(player, "Commands: new, set, delete, list");
-									inform(player, "/switchy new {name} - create a new preset");
-									inform(player, "/switchy set {name} - saves current preset and swaps to another");
-									inform(player, "/switchy delete {name} - delete a preset");
-									inform(player, "/switchy list - list all created presets");
-									return 1;
-								}))
-						.then(literal("list")
-								.executes((context) -> {
-									ServerPlayerEntity player = context.getSource().getPlayer();
-
-									SwitchyPresets presets = ((SwitchyPlayer) player).switchy$getPresets();
-									player.sendMessage(Text.literal("Presets: ").append(Text.literal(presets.toString())), false);
-									player.sendMessage(Text.literal("Current Preset: ").append(Text.literal(Objects.toString(presets.getCurrentPreset(), "<None>"))), false);
-									return 1;
-								}))
-						.then(literal("new")
-								.then(argument("preset", StringArgumentType.word())
-										.executes((context) -> {
-											ServerPlayerEntity player = context.getSource().getPlayer();
-											String presetName = context.getArgument("preset", String.class);
-											SwitchyPresets presets = validateSwitchyPlayer(player).switchy$getPresets();
-
-											if (presets.addPreset(new SwitchyPreset(presetName))) {
-												informWithPreset(player, "Successfully added empty preset ", presetName);
-											} else {
-												informWithPreset(player, "That preset already exists! - try /switchy set ", presetName);
-											}
-											return 1;
-										})))
-						.then(literal("set")
-								.then(argument("preset", StringArgumentType.word())
-										.executes((context) -> {
-											ServerPlayerEntity player = context.getSource().getPlayer();
-											String presetName = context.getArgument("preset", String.class);
-											SwitchyPresets presets = validateSwitchyPlayer(player).switchy$getPresets();
-
-											String oldPresetName = Objects.toString(presets.getCurrentPreset(), "<None>");
-											if (presets.setCurrentPreset(presetName)) {
-												informSwitch(player, oldPresetName, presetName);
-											} else {
-												inform(player, "That preset doesn't exist! /switchy list");
-											}
-											return 1;
-										})))
-						.then(literal("delete")
-								.then(argument("preset", StringArgumentType.word())
-										.executes((context) -> {
-											ServerPlayerEntity player = context.getSource().getPlayer();
-											String presetName = context.getArgument("preset", String.class);
-											SwitchyPresets presets = validateSwitchyPlayer(player).switchy$getPresets();
-
-											if (presets.deletePreset(presetName)) {
-												informWithPreset(player, "Preset deleted: ", presetName);
-											} else {
-												inform(player, "That preset doesn't exist! /switchy list");
-											}
-											return 1;
-										})))
-		));
-
+		SwitchyCommands.InitializeCommands();
 		LOGGER.info("Switchy Initialized!");
-	}
-
-	private SwitchyPlayer validateSwitchyPlayer(ServerPlayerEntity player) {
-		if (((SwitchyPlayer) player).switchy$getPresets() == null) {
-			((SwitchyPlayer) player).switchy$setPresets(SwitchyPresets.fromNbt(player, new NbtList()));
-		}
-		return (SwitchyPlayer) player;
-	}
-
-	private void informSwitch(ServerPlayerEntity player, String oldPreset, String newPreset) {
-		player.sendMessage(Text.literal("You've switched from ")
-						.setStyle(Style.EMPTY.withColor(Formatting.YELLOW))
-						.append(Text.literal(oldPreset))
-						.append(Text.literal(" to "))
-						.append(Text.literal(newPreset))
-				, false);
-	}
-
-	private void informWithPreset(ServerPlayerEntity player, String literal, String preset) {
-		player.sendMessage(Text.literal(literal)
-						.setStyle(Style.EMPTY.withColor(Formatting.YELLOW))
-						.append(Text.literal(preset))
-				, false);
-	}
-
-	private void inform(ServerPlayerEntity player, String literal) {
-		player.sendMessage(Text.literal(literal)
-						.setStyle(Style.EMPTY.withColor(Formatting.YELLOW))
-				, false);
 	}
 
 }
---
+++ src/main/java/folk/sisby/switchy/SwitchyCommands.java
@@ -0,0 +1,129 @@
+package folk.sisby.switchy;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.nbt.NbtList;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.quiltmc.qsl.command.api.CommandRegistrationCallback;
+
+import java.util.Objects;
+
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+
+public class SwitchyCommands {
+	public static void InitializeCommands() {
+		CommandRegistrationCallback.EVENT.register((dispatcher, buildContext, environment) -> dispatcher.register(
+				literal("switchy")
+						.then(literal("help")
+								.executes(SwitchyCommands::executeHelp))
+						.then(literal("list")
+								.executes(SwitchyCommands::executeList))
+						.then(literal("new")
+								.then(argument("preset", StringArgumentType.word())
+										.executes(SwitchyCommands::executeNew)))
+						.then(literal("set")
+								.then(argument("preset", StringArgumentType.word())
+										.executes(SwitchyCommands::executeSet)))
+						.then(literal("delete")
+								.then(argument("preset", StringArgumentType.word())
+										.executes(SwitchyCommands::executeDelete)))
+		));
+	}
+
+	private static int executeHelp(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		ServerPlayerEntity player = context.getSource().getPlayer();
+
+		inform(player, "Commands: new, set, delete, list");
+		inform(player, "/switchy new {name} - create a new preset");
+		inform(player, "/switchy set {name} - saves current preset and swaps to another");
+		inform(player, "/switchy delete {name} - delete a preset");
+		inform(player, "/switchy list - list all created presets");
+		return 1;
+	}
+
+	private static int executeList(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		ServerPlayerEntity player = context.getSource().getPlayer();
+
+		SwitchyPresets presets = ((SwitchyPlayer) player).switchy$getPresets();
+		player.sendMessage(Text.literal("Presets: ").append(Text.literal(presets.toString())), false);
+		player.sendMessage(Text.literal("Current Preset: ").append(Text.literal(Objects.toString(presets.getCurrentPreset(), "<None>"))), false);
+		return 1;
+	}
+
+	private static int executeNew(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		ServerPlayerEntity player = context.getSource().getPlayer();
+		String presetName = context.getArgument("preset", String.class);
+		SwitchyPresets presets = validateSwitchyPlayer(player).switchy$getPresets();
+
+		if (presets.addPreset(new SwitchyPreset(presetName))) {
+			informWithPreset(player, "Successfully added preset ", presetName);
+			return executeSet(context);
+		} else {
+			informWithPreset(player, "That preset already exists! - try /switchy set ", presetName);
+			return 1;
+		}
+	}
+
+	private static int executeSet(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		ServerPlayerEntity player = context.getSource().getPlayer();
+		String presetName = context.getArgument("preset", String.class);
+		SwitchyPresets presets = validateSwitchyPlayer(player).switchy$getPresets();
+
+		String oldPresetName = Objects.toString(presets.getCurrentPreset(), "<None>");
+		if (presets.setCurrentPreset(presetName)) {
+			informSwitch(player, oldPresetName, presetName);
+		} else {
+			inform(player, "That preset doesn't exist! /switchy list");
+		}
+		return 1;
+	}
+
+	private static int executeDelete(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+		ServerPlayerEntity player = context.getSource().getPlayer();
+		String presetName = context.getArgument("preset", String.class);
+		SwitchyPresets presets = validateSwitchyPlayer(player).switchy$getPresets();
+
+		if (presets.deletePreset(presetName)) {
+			informWithPreset(player, "Preset deleted: ", presetName);
+		} else {
+			inform(player, "That preset doesn't exist! /switchy list");
+		}
+		return 1;
+	}
+
+	private static SwitchyPlayer validateSwitchyPlayer(ServerPlayerEntity player) {
+		if (((SwitchyPlayer) player).switchy$getPresets() == null) {
+			((SwitchyPlayer) player).switchy$setPresets(SwitchyPresets.fromNbt(player, new NbtList()));
+		}
+		return (SwitchyPlayer) player;
+	}
+
+	private static void informSwitch(ServerPlayerEntity player, String oldPreset, String newPreset) {
+		player.sendMessage(Text.literal("You've switched from ")
+						.setStyle(Style.EMPTY.withColor(Formatting.YELLOW))
+						.append(Text.literal(oldPreset).setStyle(Style.EMPTY.withColor(Formatting.WHITE)))
+						.append(Text.literal(" to "))
+						.append(Text.literal(newPreset).setStyle(Style.EMPTY.withColor(Formatting.WHITE)))
+				, false);
+	}
+
+	private static void informWithPreset(ServerPlayerEntity player, String literal, String preset) {
+		player.sendMessage(Text.literal(literal)
+						.setStyle(Style.EMPTY.withColor(Formatting.YELLOW))
+						.append(Text.literal(preset).setStyle(Style.EMPTY.withColor(Formatting.WHITE)))
+				, false);
+	}
+
+	private static void inform(ServerPlayerEntity player, String literal) {
+		player.sendMessage(Text.literal(literal)
+						.setStyle(Style.EMPTY.withColor(Formatting.YELLOW))
+				, false);
+	}
+
+}
