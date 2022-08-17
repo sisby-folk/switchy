ddd4cf82cd33299871a9716dc6ce6f49546bfa3e
sisby-folk <nora.herrah.heartstrings@gmail.com>
2022-08-18 01:37:46 +1000

    Fix cherrypick.

--- src/main/java/folk/sisby/switchy/SwitchyCommands.java
+++ src/main/java/folk/sisby/switchy/SwitchyCommands.java
@@ -9,6 +9,7 @@ import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Style;
+import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import org.quiltmc.qsl.command.api.CommandRegistrationCallback;
 
