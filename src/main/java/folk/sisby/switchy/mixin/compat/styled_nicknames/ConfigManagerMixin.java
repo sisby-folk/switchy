68702f104547ff671151e3661a6b90f81aa8c82c
sisby-folk <nora.herrah.heartstrings@gmail.com>
2022-11-29 21:29:06 +1100

    Force allow hover/color for styled nicknames

--- src/main/java/folk/sisby/switchy/mixin/compat/styled_nicknames/ConfigManagerMixin.java
+++ src/main/java/folk/sisby/switchy/mixin/compat/styled_nicknames/ConfigManagerMixin.java
@@ -15,5 +15,25 @@ public abstract class ConfigManagerMixin {
 	@Inject(method = "getConfig", at = @At("RETURN"))
 	private static void forceAllowByDefault(CallbackInfoReturnable<Config> cir) {
 		CONFIG.configData.allowByDefault = true;
+		CONFIG.configData.defaultEnabledFormatting.put("hover", true);
+		CONFIG.configData.defaultEnabledFormatting.put("dark_red", true);
+		CONFIG.configData.defaultEnabledFormatting.put("yellow", true);
+		CONFIG.configData.defaultEnabledFormatting.put("dark_blue", true);
+		CONFIG.configData.defaultEnabledFormatting.put("dark_purple", true);
+		CONFIG.configData.defaultEnabledFormatting.put("gold", true);
+		CONFIG.configData.defaultEnabledFormatting.put("red", true);
+		CONFIG.configData.defaultEnabledFormatting.put("aqua", true);
+		CONFIG.configData.defaultEnabledFormatting.put("gray", true);
+		CONFIG.configData.defaultEnabledFormatting.put("light_purple", true);
+		CONFIG.configData.defaultEnabledFormatting.put("white", true);
+		CONFIG.configData.defaultEnabledFormatting.put("dark_gray", true);
+		CONFIG.configData.defaultEnabledFormatting.put("dark_grey", true);
+		CONFIG.configData.defaultEnabledFormatting.put("green", true);
+		CONFIG.configData.defaultEnabledFormatting.put("dark_green", true);
+		// CONFIG.configData.defaultEnabledFormatting.put("black", true); Drogtor's Choice
+		CONFIG.configData.defaultEnabledFormatting.put("grey", true);
+		CONFIG.configData.defaultEnabledFormatting.put("orange", true);
+		CONFIG.configData.defaultEnabledFormatting.put("blue", true);
+		CONFIG.configData.defaultEnabledFormatting.put("dark_aqua", true);
 	}
 }
