49d0fb5e13bba4de9c77cab96ef2cef0b72e639e
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-02-25 17:43:03 +1100

    javadoc batch 4 CORE IS DONE

--- core/src/main/java/folk/sisby/switchy/api/presets/SwitchyPresets.java
+++ core/src/main/java/folk/sisby/switchy/api/presets/SwitchyPresets.java
@@ -18,10 +18,8 @@ import java.util.function.Consumer;
  */
 public interface SwitchyPresets extends SwitchyPresetsData<SwitchyModule, SwitchyPreset> {
 	/**
+	 * {@inheritDoc}
 	 * @param player the player this presets object belongs to
-	 * @param other a map of presets to import into this object
-	 * Imports a set of presets, merging by replacing modules where preset names collide.
-	 * Only registered, enabled modules will be imported.
 	 * Will hot-modify the current preset using {@link SwitchyPresets#duckCurrentModule(ServerPlayerEntity, Identifier, Consumer)}
 	 */
 	void importFromOther(ServerPlayerEntity player, Map<String, SwitchyPreset> other);
@@ -35,8 +33,8 @@ public interface SwitchyPresets extends SwitchyPresetsData<SwitchyModule, Switch
 	 * @param player the player this presets object belongs to
 	 * @param name the case-insensitive name of a preset
 	 * @return the (case-corrected) name of the new current preset
-	 * @throws IllegalArgumentException when a preset with that name doesn't exist
-	 * @throws IllegalStateException when the preset with that name is already the current preset
+	 * @throws IllegalArgumentException when a preset with the specified name doesn't exist
+	 * @throws IllegalStateException when the preset with the specified name is the current preset
 	 * "Switches" from the current preset to the specified one.
 	 * All module-specified player data is saved to the current preset, then all module-specified data is loaded from the specified preset.
 	 * The current preset will be updated to the specified preset.
@@ -54,7 +52,7 @@ public interface SwitchyPresets extends SwitchyPresetsData<SwitchyModule, Switch
 	 * @param player the player this presets object belongs to
 	 * @param id a module identifier
 	 * @param mutator a consumer that will modify the module while ducked
-	 * @throws IllegalArgumentException when a module with that ID doesn't exist
+	 * @throws IllegalArgumentException when the specified module doesn't exist
 	 * @throws IllegalStateException when the specified module is disabled
 	 * Allows you to "hot modify" the usually inaccessible current-preset data for the specified module.
 	 * Achieves this by saving the module, mutating it, then restoring it all in one swoop.
--- core/src/main/java/folk/sisby/switchy/api/presets/SwitchyPresetsData.java
+++ core/src/main/java/folk/sisby/switchy/api/presets/SwitchyPresetsData.java
@@ -11,7 +11,8 @@ import java.util.Map;
 /**
  * @author Sisby folk
  * @since 2.0.0
- * A collection of {@link SwitchyPreset}, holding data, a reference for their {@link SwitchySerializable} modules
+ * A collection of {@link SwitchyPreset}, holding data, and a reference to their {@link SwitchySerializable} modules.
+ * All contained presets have identical modules (called "enabled" modules).
  */
 public interface SwitchyPresetsData<Module extends SwitchySerializable, Preset extends SwitchyPresetData<Module>> extends SwitchySerializable {
 	String KEY_PRESET_CURRENT = "current";
@@ -20,155 +21,166 @@ public interface SwitchyPresetsData<Module extends SwitchySerializable, Preset e
 	String KEY_PRESET_LIST = "list";
 
 	/**
-	 * @param other
+	 * @param other a map of presets to import into this object
+	 * Imports a set of presets, merging by replacing modules where preset names collide.
+	 * Only registered, enabled modules will be imported.
 	 */
 	void importFromOther(Map<String, Preset> other);
 
 	/**
-	 * @param name
-	 * @throws IllegalArgumentException
+	 * @param name the case-insensitive name of a preset
+	 * @throws IllegalArgumentException when a preset with the specified name doesn't exist
 	 */
+	@ApiStatus.Internal
 	void setCurrentPreset(String name) throws IllegalArgumentException;
 
 	/**
-	 * @param preset
-	 * @throws IllegalStateException
+	 * @param preset a named preset object
+	 * @throws IllegalStateException when a preset with the provided name already exists
 	 */
 	void addPreset(Preset preset) throws IllegalStateException;
 
 	/**
-	 * @param name
-	 * @return
-	 * @throws IllegalStateException
+	 * @param name the case-insensitive name of a preset
+	 * @return the newly created preset
+	 * @throws IllegalStateException when a preset with the provided name already exists
 	 */
 	Preset newPreset(String name) throws IllegalStateException;
 
 	/**
-	 * @param name
-	 * @param dryRun
-	 * @throws IllegalArgumentException
-	 * @throws IllegalStateException
+	 * @param name the case-insensitive name of a preset
+	 * @param dryRun whether to skip deleting the preset
+	 * @throws IllegalArgumentException when a preset with the specified name doesn't exist
+	 * @throws IllegalStateException when the preset with the specified name is the current preset
+	 * @see SwitchyPresets#deletePreset(String)
+	 * For use in situations where throwable-based validation is desired before confirming the action.
 	 */
 	@ApiStatus.Internal
 	void deletePreset(String name, boolean dryRun) throws IllegalArgumentException, IllegalStateException;
 
 	/**
-	 * @param name
-	 * @throws IllegalArgumentException
-	 * @throws IllegalStateException
+	 * @param name the case-insensitive name of a preset
+	 * @throws IllegalArgumentException when a preset with the specified name doesn't exist
+	 * @throws IllegalStateException  when the preset with the specified name is the current preset
+	 * Deletes the specified preset, along with any associated data.
 	 */
 	void deletePreset(String name) throws IllegalArgumentException, IllegalStateException;
 
 	/**
 	 * @param name the case-insensitive name of a preset
 	 * @param newName the new name for the specified preset. a single word matching {@code azAZ09_-.+}
-	 * @throws IllegalArgumentException when a preset with that name doesn't exist
-	 * @throws IllegalStateException when a preset with the desired new name already exists
+	 * @throws IllegalArgumentException when a preset with the specified name doesn't exist
+	 * @throws IllegalStateException when a preset with the provided name already exists
+	 * Safely changes the name of the specified preset.
 	 */
 	void renamePreset(String name, String newName) throws IllegalArgumentException, IllegalStateException;
 
 	/**
-	 * @param id
-	 * @param dryRun
-	 * @throws IllegalArgumentException
-	 * @throws IllegalStateException
+	 * @param id a module identifier
+	 * @param dryRun whether to skip disabling the module
+	 * @throws IllegalArgumentException when the specified module doesn't exist
+	 * @throws IllegalStateException when the specified module is disabled
+	 * @see SwitchyPresets#disableModule(Identifier)
+	 * For use in situations where throwable-based validation is desired before confirming the action.
 	 */
 	@ApiStatus.Internal
 	void disableModule(Identifier id, boolean dryRun) throws IllegalArgumentException, IllegalStateException;
 
 	/**
-	 * @param id
-	 * @throws IllegalArgumentException
-	 * @throws IllegalStateException
+	 * @param id a module identifier
+	 * @throws IllegalArgumentException when the specified module doesn't exist
+	 * @throws IllegalStateException when the specified module is disabled
+	 * Disables a module, deleting all instances of it across presets
 	 */
 	void disableModule(Identifier id) throws IllegalArgumentException, IllegalStateException;
 
 	/**
-	 * @param id
-	 * @throws IllegalArgumentException
-	 * @throws IllegalStateException
+	 * @param id a module identifier
+	 * @throws IllegalArgumentException when the specified module doesn't exist
+	 * @throws IllegalStateException when the specified module is enabled
+	 * Enables a module, creating empty instances in every preset
 	 */
 	void enableModule(Identifier id) throws IllegalArgumentException, IllegalStateException;
 
 	/**
-	 * @return
+	 * @return the current preset
 	 */
 	@ApiStatus.Internal
 	Preset getCurrentPreset();
 
 	/**
-	 * @return
+	 * @return the name of the current preset
 	 */
 	String getCurrentPresetName();
 
 	/**
-	 * @return
+	 * @return all contained presets
 	 */
 	@ApiStatus.Internal
 	Map<String, Preset> getPresets();
 
 	/**
-	 * @param name
-	 * @return
+	 * @param name the case-insensitive name of a preset
+	 * @return the specified preset
 	 */
 	@ApiStatus.Internal
 	Preset getPreset(String name);
 
 	/**
-	 * @return
+	 * @return a list of each preset name
 	 */
 	List<String> getPresetNames();
 
 	/**
-	 * @param name
-	 * @return
+	 * @param name the case-insensitive name of a preset
+	 * @return whether that preset exists
 	 */
 	boolean containsPreset(String name);
 
 	/**
-	 * @return
+	 * @return the enabled status of all modules
 	 */
 	@ApiStatus.Internal
 	Map<Identifier, Boolean> getModules();
 
 	/**
-	 * @param id
-	 * @return
-	 * @throws IllegalArgumentException
-	 * @throws IllegalStateException
+	 * @param id a module identifier
+	 * @return a map of each preset to the specified module
+	 * @throws IllegalArgumentException when the specified module doesn't exist
+	 * @throws IllegalStateException when the specified module is disabled
 	 */
 	Map<String, Module> getAllOfModule(Identifier id) throws IllegalArgumentException, IllegalStateException;
 
 	/**
-	 * @return
+	 * @return a list of all enabled module IDs
 	 */
 	List<Identifier> getEnabledModules();
 
 	/**
-	 * @return
+	 * @return a list of all disabled module IDs
 	 */
 	List<Identifier> getDisabledModules();
 
 	/**
-	 * @param id
-	 * @return
+	 * @param id a module identifier
+	 * @return whether a module is registered at all in this object
 	 */
 	boolean containsModule(Identifier id);
 
 	/**
-	 * @param id
-	 * @return
-	 * @throws IllegalArgumentException
+	 * @param id a module identifier
+	 * @return whether the specified module is enabled
+	 * @throws IllegalArgumentException when the specified module doesn't exist
 	 */
 	boolean isModuleEnabled(Identifier id) throws IllegalArgumentException;
 
 	/**
-	 * @return
+	 * @return a list of all enabled module names (paths)
 	 */
 	List<String> getEnabledModuleNames();
 
 	/**
-	 * @return
+	 * @return a text representation of all module IDs, showing paths with full IDs on hover
 	 */
 	MutableText getEnabledModuleText();
 
