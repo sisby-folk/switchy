5352dffe0b3f2b2dbdcdd053c3686eb318e758bc
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-03-11 00:23:28 +1100

    simplify displaymodule interface

--- ui/src/main/java/folk/sisby/switchy/ui/api/module/SwitchyDisplayModule.java
+++ ui/src/main/java/folk/sisby/switchy/ui/api/module/SwitchyDisplayModule.java
@@ -1,24 +1,20 @@
 package folk.sisby.switchy.ui.api.module;
 
 import com.mojang.datafixers.util.Pair;
-import folk.sisby.switchy.api.SwitchySerializable;
-import folk.sisby.switchy.api.module.SwitchyModuleClientable;
 import folk.sisby.switchy.ui.api.SwitchySwitchScreenPosition;
 import io.wispforest.owo.ui.core.Component;
 import org.jetbrains.annotations.Nullable;
 import org.quiltmc.loader.api.minecraft.ClientOnly;
 
 /**
- * Holds a client-appropriate representation of the contents of a Switchy Module.
- * The represented module must implement {@link SwitchyModuleClientable}.
+ * A type of client module that can be rendered on the Switchy UI switch screen.
  *
  * @author Sisby folk
- * @see SwitchySerializable
- * @see folk.sisby.switchy.api.module.SwitchyModule
+ * @see folk.sisby.switchy.client.api.module.SwitchyClientModule
  * @since 2.0.0
  */
 @ClientOnly
-public interface SwitchyDisplayModule extends SwitchySerializable {
+public interface SwitchyDisplayModule {
 	/**
 	 * @return a renderable component, and the position on the preset preview it should be placed.
 	 */
