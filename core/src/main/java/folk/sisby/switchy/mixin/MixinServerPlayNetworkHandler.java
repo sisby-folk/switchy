d3e3839d9e84967d2f2c709ea7d25fa287055828
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-06-17 13:16:39 +1000

    fix fix

--- core/src/main/java/folk/sisby/switchy/mixin/MixinServerPlayNetworkHandler.java
+++ core/src/main/java/folk/sisby/switchy/mixin/MixinServerPlayNetworkHandler.java
@@ -1,6 +1,7 @@
 package folk.sisby.switchy.mixin;
 
 import folk.sisby.switchy.SwitchyCommands;
+import net.minecraft.network.message.MessageSignatureList;
 import net.minecraft.network.packet.c2s.play.ChatCommandC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import org.spongepowered.asm.mixin.Mixin;
@@ -19,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class MixinServerPlayNetworkHandler {
 	@SuppressWarnings("DataFlowIssue")
 	@Inject(at = @At(value = "TAIL"), method = "method_45010")
-	void onChatCommand(ChatCommandC2SPacket packet, CallbackInfo ci) {
+	void onChatCommand(ChatCommandC2SPacket packet, MessageSignatureList signatureList, CallbackInfo ci) {
 		SwitchyCommands.HISTORY.put(((ServerPlayNetworkHandler) (Object) this).player.getUuid(), packet.command());
 	}
 }
