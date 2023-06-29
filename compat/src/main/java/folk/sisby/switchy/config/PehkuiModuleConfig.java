972ba39872032a7cd39e51a82f2949a036ccdae8
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-06-29 17:38:56 +1000

    configurable /power support

--- compat/src/main/java/folk/sisby/switchy/config/PehkuiModuleConfig.java
+++ compat/src/main/java/folk/sisby/switchy/config/PehkuiModuleConfig.java
@@ -7,15 +7,15 @@ import virtuoel.pehkui.api.ScaleRegistries;
 import virtuoel.pehkui.api.ScaleTypes;
 
 /**
- * Handles module cold-editing permission configuration (including importing).
- * Saved as {@code config/switchy/config.toml}.
+ * Handles scale-type configuration for the Pehkui module.
+ * Saved as {@code config/switchy/pehkui.toml}.
  *
  * @author Sisby folk
  * @since 2.2.0
  */
 public class PehkuiModuleConfig extends WrappedConfig {
 	/**
-	 * Editable list of scale types that the Pehkui module will switch .
+	 * Editable list of scale types that the Pehkui module will switch.
 	 */
 	@Comment("Scale types that the pehkui module will switch on this server.")
 	public final ValueList<String> scaleTypes = ValueList.create(
--- compat/src/main/java/folk/sisby/switchy/modules/ApoliModule.java
+++ compat/src/main/java/folk/sisby/switchy/modules/ApoliModule.java
@@ -5,6 +5,8 @@ import folk.sisby.switchy.api.module.SwitchyModule;
 import folk.sisby.switchy.api.module.SwitchyModuleEditable;
 import folk.sisby.switchy.api.module.SwitchyModuleInfo;
 import folk.sisby.switchy.api.module.SwitchyModuleRegistry;
+import folk.sisby.switchy.config.ApoliModuleConfig;
+import folk.sisby.switchy.config.PehkuiModuleConfig;
 import io.github.apace100.apoli.component.PowerHolderComponent;
 import io.github.apace100.apoli.power.InventoryPower;
 import io.github.apace100.apoli.power.Power;
@@ -13,14 +15,13 @@ import io.github.apace100.apoli.power.PowerTypeRegistry;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.nbt.NbtList;
+import net.minecraft.nbt.NbtString;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
+import org.quiltmc.loader.api.config.QuiltConfig;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static folk.sisby.switchy.util.Feedback.translatable;
 
@@ -36,7 +37,15 @@ public class ApoliModule implements SwitchyModule {
 	 * Identifier for this module.
 	 */
 	public static final Identifier ID = new Identifier("switchy", "apoli");
-
+	/**
+	 * The config object for the apoli module, containing the current state of {@code /config/switchy/apoli.toml}.
+	 */
+	public static final ApoliModuleConfig CONFIG = QuiltConfig.create(Switchy.ID, "apoli", ApoliModuleConfig.class);
+	private static final Identifier COMMAND_SOURCE = new Identifier("apoli", "command");
+	/**
+	 * The NBT key where the list of serialized apoli:command powers are stored.
+	 */
+	public static final String KEY_COMMAND_POWERS = "CommandPowers";
 	/**
 	 * The NBT key where the list of power data is stored.
 	 */
@@ -55,6 +64,10 @@ public class ApoliModule implements SwitchyModule {
 	}
 
 	/**
+	 * Powers added by commands allowed to be switched
+	 */
+	public List<PowerType<?>> commandPowers = null;
+	/**
 	 * The NBT data for each power.
 	 */
 	public final Map<PowerType<?>, NbtElement> powerNbt = new HashMap<>();
@@ -71,9 +84,17 @@ public class ApoliModule implements SwitchyModule {
 
 	@Override
 	public void updateFromPlayer(ServerPlayerEntity player, @Nullable String nextPreset) {
+		PowerHolderComponent playerHolder = PowerHolderComponent.KEY.get(player);
+		if (CONFIG.switchCommandPowers || !CONFIG.exceptionPowerIds.isEmpty()) {
+			commandPowers = new ArrayList<>();
+			for (PowerType<?> powerType : playerHolder.getPowersFromSource(COMMAND_SOURCE)) {
+				if (CONFIG.canSwitchPower(powerType) && !commandPowers.contains(powerType)) {
+					commandPowers.add(powerType);
+				}
+			}
+		}
 		powerNbt.clear();
-		List<Power> powers = PowerHolderComponent.KEY.get(player).getPowers();
-		for (Power power : powers) {
+		for (Power power : playerHolder.getPowers()) {
 			powerNbt.put(power.getType(), power.toTag());
 		}
 		if (nextPreset != null) {
@@ -83,8 +104,21 @@ public class ApoliModule implements SwitchyModule {
 
 	@Override
 	public void applyToPlayer(ServerPlayerEntity player) {
+		PowerHolderComponent playerHolder = PowerHolderComponent.KEY.get(player);
+		if (commandPowers != null) {
+			playerHolder.getPowersFromSource(COMMAND_SOURCE).forEach(powerType -> {
+				if (CONFIG.canSwitchPower(powerType) && !commandPowers.contains(powerType)) {
+					playerHolder.removePower(powerType, COMMAND_SOURCE);
+				}
+			});
+			commandPowers.forEach(powerType -> {
+				if (CONFIG.canSwitchPower(powerType) && !playerHolder.hasPower(powerType, COMMAND_SOURCE)) {
+					playerHolder.addPower(powerType, COMMAND_SOURCE);
+				}
+			});
+		}
 		powerNbt.forEach((powerType, nbt) -> {
-			Power power = PowerHolderComponent.KEY.get(player).getPower(powerType);
+			Power power = playerHolder.getPower(powerType);
 			if (power != null) {
 				power.fromTag(nbt);
 			}
@@ -99,6 +133,11 @@ public class ApoliModule implements SwitchyModule {
 	@Override
 	public NbtCompound toNbt() {
 		NbtCompound outNbt = new NbtCompound();
+		if (commandPowers != null) {
+			NbtList commandPowerList = new NbtList();
+			commandPowers.forEach(powerType -> commandPowerList.add(NbtString.of(powerType.getIdentifier().toString())));
+			outNbt.put(KEY_COMMAND_POWERS, commandPowerList);
+		}
 		NbtList powerNbtList = new NbtList();
 		powerNbt.forEach((powerType, nbt) -> {
 			NbtCompound powerTag = new NbtCompound();
@@ -112,6 +151,18 @@ public class ApoliModule implements SwitchyModule {
 
 	@Override
 	public void fillFromNbt(NbtCompound nbt) {
+		if (nbt.contains(KEY_COMMAND_POWERS, NbtElement.LIST_TYPE)) {
+			NbtList commandPowerList = nbt.getList(KEY_COMMAND_POWERS, NbtElement.STRING_TYPE);
+			commandPowers = new ArrayList<>();
+			commandPowerList.forEach(id -> {
+				try {
+					PowerType<?> powerType = PowerTypeRegistry.get(Identifier.tryParse(id.asString()));
+					commandPowers.add(powerType);
+				} catch (IllegalArgumentException powerGetEx) {
+					Switchy.LOGGER.warn("[Switchy] Failed to load preset command power with id {}. Exception: {}", id, powerGetEx);
+				}
+			});
+		}
 		powerNbt.clear();
 		if (nbt.contains(KEY_POWER_DATA_LIST, NbtElement.LIST_TYPE)) {
 			NbtList powerDataList = nbt.getList(KEY_POWER_DATA_LIST, NbtElement.COMPOUND_TYPE);
