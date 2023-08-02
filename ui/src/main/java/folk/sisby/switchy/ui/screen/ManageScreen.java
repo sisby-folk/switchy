aa609ac1200e030c3b3313f36b3376e32c5a94c8
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-08-02 20:37:47 +1000

    update

--- ui/src/main/java/folk/sisby/switchy/ui/screen/ManageScreen.java
+++ ui/src/main/java/folk/sisby/switchy/ui/screen/ManageScreen.java
@@ -155,7 +155,7 @@ public class ManageScreen extends BaseOwoScreen<LockableFlowLayout> implements S
 			TextBoxComponent nameEntry = Components.textBox(Sizing.fill(53), (presetName != null) ? presetName : "newPreset");
 			nameEntry.setTextPredicate(s -> s.chars().mapToObj(i -> (char) i).allMatch(StringReader::isAllowedInUnquotedString));
 			nameEntry.onChanged();
-			ManageScreen.this.setInitialFocus(nameEntry);
+			ManageScreen.this.setFocusedChild(nameEntry);
 			renamePresetFlow.child((Component) nameEntry);
 			ButtonComponent confirmButton = Components.button(Text.translatable("screen.switchy.manage.presets.confirm"), (presetName != null) ? b -> {
 				if (!presetName.equals(nameEntry.getText())) {
