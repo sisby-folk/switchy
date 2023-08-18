951253f202f207b2243aa996f8b6a5554672e452
sisby-folk <nora.herrah.heartstrings@gmail.com>
2023-08-18 11:33:25 +1000

    no remapping for figura!

--- compat/src/main/java/folk/sisby/switchy/mixin/compat/figura/AvatarMixin.java
+++ compat/src/main/java/folk/sisby/switchy/mixin/compat/figura/AvatarMixin.java
@@ -7,7 +7,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Avatar.class)
+@Mixin(value = Avatar.class, remap = false)
 public class AvatarMixin {
 	@Inject(method = "clean", at = @At("HEAD"))
 	public void clean(CallbackInfo ci) {
