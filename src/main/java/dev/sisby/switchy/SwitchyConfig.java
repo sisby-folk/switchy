75e5472b83ef8a79ff8991f3bac180388b6b5363
sisby-folk <sleepingdragoninn@gmail.com>
2025-11-23 16:23:59 +1100

    LAN warning

--- src/main/java/dev/sisby/switchy/SwitchyConfig.java
+++ src/main/java/dev/sisby/switchy/SwitchyConfig.java
@@ -6,5 +6,6 @@ import folk.sisby.kaleido.lib.quiltconfig.api.annotations.Comment;
 public class SwitchyConfig extends WrappedConfig {
 	@Comment("Prevents the singleplayer server from shutting down during a kick-style (1.20 and below) switch.")
 	@Comment("May cause compatibility issues - if you crash when switching in singleplayer, turn this off.")
+	@Comment("Must be enabled to switch as a LAN host.")
 	public boolean fastSingleplayerReconnect = true;
 }
