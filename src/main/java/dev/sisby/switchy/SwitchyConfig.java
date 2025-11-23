6301bc6fecffc38e3f81a135a08cb1b5a861605c
sisby-folk <sleepingdragoninn@gmail.com>
2025-11-23 16:24:33 +1100

    no config option on 1.21

--- src/main/java/dev/sisby/switchy/SwitchyConfig.java
+++ src/main/java/dev/sisby/switchy/SwitchyConfig.java
@@ -1,11 +1,6 @@
 package dev.sisby.switchy;
 
 import folk.sisby.kaleido.api.WrappedConfig;
-import folk.sisby.kaleido.lib.quiltconfig.api.annotations.Comment;
 
 public class SwitchyConfig extends WrappedConfig {
-	@Comment("Prevents the singleplayer server from shutting down during a kick-style (1.20 and below) switch.")
-	@Comment("May cause compatibility issues - if you crash when switching in singleplayer, turn this off.")
-	@Comment("Must be enabled to switch as a LAN host.")
-	public boolean fastSingleplayerReconnect = true;
 }
