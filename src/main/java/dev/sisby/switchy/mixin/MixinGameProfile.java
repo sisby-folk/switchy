ee27670ab864933303857c779bbb1183eb6e6f5a
sisby-folk <sleepingdragoninn@gmail.com>
2026-02-24 17:57:20 +1100

    theoretical bridge avatar support

---
+++ src/main/java/dev/sisby/switchy/mixin/MixinGameProfile.java
@@ -0,0 +1,42 @@
+package dev.sisby.switchy.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import com.mojang.authlib.properties.PropertyMap;
+import dev.sisby.switchy.data.SwitchyComponentType;
+import dev.sisby.switchy.data.SwitchyComponentTypes;
+import dev.sisby.switchy.data.SwitchyProfile;
+import dev.sisby.switchy.duck.SwitchyGameProfile;
+import net.minecraft.nbt.NbtCompound;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value = GameProfile.class, remap = false)
+public class MixinGameProfile implements SwitchyGameProfile {
+	private SwitchyProfile switchy$sayProfile = null;
+
+	@Override
+	public SwitchyProfile switchy$getSayProfile() {
+		return switchy$sayProfile;
+	}
+
+	@Override
+	public void switchy$setSayProfile(SwitchyProfile profile) {
+		switchy$sayProfile = profile;
+	}
+
+	@ModifyReturnValue(method = "getProperties", at = @At("RETURN"))
+	private PropertyMap overrideSkinDuringSayForBridges(PropertyMap original) {
+		SwitchyComponentType<NbtCompound> skin = (SwitchyComponentType<NbtCompound>) SwitchyComponentTypes.instance().get(SwitchyComponentTypes.TAILOR_SKIN);
+		if (skin != null && switchy$sayProfile != null && switchy$sayProfile.contains(skin)) {
+			NbtCompound compound = switchy$sayProfile.get(skin);
+			PropertyMap newMap = new PropertyMap();
+			newMap.putAll(original);
+			newMap.removeAll("textures");
+			newMap.put("textures", new Property("textures", compound.getString("value"), compound.getString("signature")));
+			return newMap;
+		}
+		return original;
+	}
+}
--- src/main/java/dev/sisby/switchy/mixin/MixinServerPlayerEntity.java
+++ src/main/java/dev/sisby/switchy/mixin/MixinServerPlayerEntity.java
@@ -2,7 +2,6 @@ package dev.sisby.switchy.mixin;
 
 import dev.sisby.switchy.Switchy;
 import dev.sisby.switchy.data.SwitchyPlayerData;
-import dev.sisby.switchy.data.SwitchyProfile;
 import dev.sisby.switchy.duck.SwitchyPlayer;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.server.network.ServerPlayerEntity;
@@ -15,7 +14,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayerEntity.class)
 public class MixinServerPlayerEntity implements SwitchyPlayer {
 	private SwitchyPlayerData switchy$playerData = null;
-	private SwitchyProfile switchy$sayProfile = null;
 	private NbtCompound switchy$hotSwap = null;
 	private NbtCompound switchy$reloadData = null;
 
@@ -64,16 +62,6 @@ public class MixinServerPlayerEntity implements SwitchyPlayer {
 	@Override
 	public SwitchyPlayerData switchy$getPlayerData() {
 		return switchy$playerData;
-	}
-
-	@Override
-	public SwitchyProfile switchy$getSayProfile() {
-		return switchy$sayProfile;
-	}
-
-	@Override
-	public void switchy$setSayProfile(SwitchyProfile profile) {
-		switchy$sayProfile = profile;
 	}
 
 	@Inject(method = "readCustomDataFromNbt", at = @At("TAIL"))
